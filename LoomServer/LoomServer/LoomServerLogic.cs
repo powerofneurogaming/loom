7e70ac625f7ba1fc4dce9d70b8e8c11169f6ee92
robinzhx <zhx068@ucsd.edu>
2020-11-13 04:51:27 -0800

    Fixed server retrieve data back from client - Forget to write loop logic for server

---
+++ LoomServer/LoomServer/LoomServerLogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoomServer
+{
+    class LoomServerLogic
+    {
+        public static void Update()
+        {
+            ThreadManager.UpdateMain();
+        }
+    }
+}
--- LoomServer/LoomServer/Program.cs
+++ LoomServer/LoomServer/Program.cs
@@ -1,17 +1,43 @@
 ﻿using System;
+using System.Threading;
 
 namespace LoomServer
 {
     class Program
     {
+        private static bool isRunning = false;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Loom Server";
+            isRunning = true;
+
+            Thread mainThread = new Thread(new ThreadStart(MainThread));
+            mainThread.Start();
 
             //https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
             Server.Start(50, 26950);
+        }
 
-            Console.ReadKey();
+        private static void MainThread()
+        {
+            Console.WriteLine($"Main thread started. Running at {Constants.TICKS_PER_SEC} ticks per second.");
+            DateTime _nextLoop = DateTime.Now;
+
+            while (isRunning)
+            {
+                while (_nextLoop < DateTime.Now)
+                {
+                    LoomServerLogic.Update();
+
+                    _nextLoop = _nextLoop.AddMilliseconds(Constants.MS_PER_TICK);
+
+                    if (_nextLoop > DateTime.Now)
+                    {
+                        Thread.Sleep(_nextLoop - DateTime.Now);
+                    }
+                }
+            }
         }
     }
 }
